bfeb72fb4da571607e7354f38e38b69e266ff5d5
kms0946 <gghuly67@gmail.com>
2023-08-23 20:51:54 +0900

    페이징 기능 구현, 수정시 페이지 위치 저장은 빠른 시일 내에 추가 예정

--- src/main/java/com/project/domain/post/PostController.java
+++ src/main/java/com/project/domain/post/PostController.java
@@ -8,13 +8,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
+
+    // 쿼리 스트링 파라미터를 Map에 담아 반환
+    private Map<String, Object> queryParamsToMap(final SearchDto queryParams) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("page", queryParams.getPage());
+        data.put("recordSize", queryParams.getRecordSize());
+        data.put("pageSize", queryParams.getPageSize());
+        data.put("keyword", queryParams.getKeyword());
+        data.put("searchType", queryParams.getSearchType());
+        return data;
+    }
 
     @GetMapping("/post/write.do")
     public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model) {
@@ -47,18 +61,20 @@ public class PostController {
     }
 
     @PostMapping("/post/update.do")
-    public String updatePost(final PostRequest params,Model model){
+    public String updatePost(final PostRequest params,final SearchDto queryParams,Model model){
         postService.updatePost(params);
-        MessageDto message=new MessageDto("게시글 수정이 완료되었습니다.","/post/list.do", RequestMethod.GET,null);
+        MessageDto message=new MessageDto("게시글 수정이 완료되었습니다.","/post/list.do", RequestMethod.GET,queryParamsToMap(queryParams));
         return showMessageAndRedirect(message,model);
     }
 
+    // 게시글 삭제
     @PostMapping("/post/delete.do")
-    public String deletePost(@RequestParam final Long id,Model model){
+    public String deletePost(@RequestParam final Long id, final SearchDto queryParams, Model model) {
         postService.deletePost(id);
-        MessageDto message=new MessageDto("게시글 삭제가 완료되었습니다.","/post/list.do", RequestMethod.GET,null);
-        return showMessageAndRedirect(message,model);
+        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, queryParamsToMap(queryParams));
+        return showMessageAndRedirect(message, model);
     }
+
 
     private String showMessageAndRedirect(final MessageDto params, Model model) {
         model.addAttribute("params", params);
